bf064fc25ace7f028e07b412215335bd971791af
Rob Tierolff <teh@borreh.nl>
2015-03-23 22:55:31 +0100

    NOT QUITE ENOUGH WINDOWS 95 YET YOU SAY? PRESS ANY KEY TO CONINUE! (im sober)

--- static/automaat.js
+++ static/automaat.js
@@ -1,41 +1,117 @@
 var results = $("#results ul");
 
+function sorty(columnname) {
+	$(".sort").removeClass("selected");
+	var selected = $("#" + columnname + ".sort");
+	selected.addClass("selected");
+	var isintsort = selected.hasClass("int");
+	var classname = ".column." + columnname;
+	var counter = 0;
+
+	$("li", results).sort(function(left, right) {
+		var leftvalue = $(classname, left).text();
+		var rightvalue = $(classname, right).text();
+
+		if (isintsort) {
+			leftvalue = parseInt(leftvalue) || 0;
+			rightvalue = parseInt(rightvalue) || 0;
+
+			if (leftvalue > rightvalue)
+				return 1;
+			if (leftvalue < rightvalue)
+				return -1;
+			return 0;
+		}
+
+		return leftvalue.localeCompare(rightvalue);
+	}).each(function() {
+		counter++;
+		var item = $(this);
+		item.remove();
+		item.appendTo(results);
+	});
+
+	$("#sorteditems").text("" + counter + " items")	
+}
+
+function viewy (viewmode) {
+	$(".view").removeClass("selected");
+	$("#" + viewmode + ".view").addClass("selected");
+
+	$("#results .viewswitch").remove();
+	$("#results").prepend('<span class="viewswitch ' + viewmode + '"></span>');
+}
+
 $("a.sort").each(function() {
 	var button = $(this);
-	var isintsort = button.hasClass("int");
+	var kenmerk = button.attr("id");
 
 	button.click(function() {
-		var columnname = button.attr("id");
-		var classname = ".column." + columnname;
-		var counter = 0;
-
-		$("li", results).sort(function(left, right) {
-			var leftvalue = $(classname, left).text();
-			var rightvalue = $(classname, right).text();
-
-			if (isintsort) {
-				leftvalue = parseInt(leftvalue) || 0;
-				rightvalue = parseInt(rightvalue) || 0;
-
-				return leftvalue > rightvalue;
-			}
-
-			return leftvalue.localeCompare(rightvalue);
-		}).each(function() {
-			counter++;
-			var item = $(this);
-			item.remove();
-			item.appendTo(results);
-		});
-
-		$("#sorteditems").text("" + counter + " items")
+		sorty(kenmerk);
+		$.cookie("sortmode", kenmerk)
 	});
 });
 
-var currentclass = ""
-
 $("a.view").click(function() {
-	var id = $(this).attr("id");
-	$("#results .viewswitch").remove();
-	$("#results").prepend('<span class="viewswitch ' + id + '"></span>');
+	var kenmerk = $(this).attr("id");
+	viewy(kenmerk);
+	$.cookie("viewmode", kenmerk);
+});
+
+if ($.cookie("sortmode") !== undefined) 
+	sorty($.cookie("sortmode"));
+
+if ($.cookie("viewmode") !== undefined)
+	viewy($.cookie("viewmode"));
+
+var mousex; var mousey;
+var offsetx; var offsety;
+var orposx; var orposy;
+var movingwindow = "x";
+
+$(document).mousemove(function(e) {
+	mousex = e.pageX;
+	mousey = e.pageY;
+
+	if (movingwindow != "x") {
+		var l = (orposx + (mousex - offsetx));
+		var t = (orposy + (mousey - offsety));
+
+		movingwindow.css({
+			left: l + "px",
+			top: t + "px"
+		});
+	}
+})
+
+$(document).mouseup(function(e) {
+	movingwindow = "x";
+});
+
+function clickety(hwnd) {
+	$(".titlebar", hwnd).mousedown(function(e) {
+		$(".window").removeClass("active");
+		hwnd.addClass("active")
+
+		offsetx = mousex;
+		offsety = mousey;
+		orposx = parseInt(hwnd.css("left"), 10);
+		orposy = parseInt(hwnd.css("top"), 10);
+
+		movingwindow = hwnd;
+	});
+}
+
+$(".window").each(function() {
+	var hwnd = $(this);
+
+	clickety(hwnd);
+});
+
+$(".navwin").click(function() {
+	var nw = $('<div class="window"><div class="vuilehack"><div class="titlebar"><img class="icon" src="/static/folder.png"><span class="caption">Mapje</span><a class="kruisje"><img src="/static/kruisje.png"></a></div><iframe src="/navigate" scrolling="no"></iframe></div></div>');
+	nw.appendTo($(".desktop"));
+	clickety(nw);
+
+ 	return false;
 });
